53d0c567256c1c7fab5130b586fc456076cafbb5
conian <conian2505@gmail.com>
2023-07-16 22:43:16 +0700

    update error role user

--- src/api/middlewares/role.middleware.ts
+++ src/api/middlewares/role.middleware.ts
@@ -1,23 +1,23 @@
 import { NextFunction, Request, Response } from "express";
 import { errorResponse } from "../routers/response";
 import { IUserAuthInfoRequest } from "../types/interface";
+import { EUserRole } from "src/database/entities/interfaces/user.interface";
 
 class RoleMiddleware {
-  async isAdmin(req:IUserAuthInfoRequest, res:Response, next:NextFunction) {
+  async isAdmin(req: IUserAuthInfoRequest, res: Response, next: NextFunction) {
     try {
       const user = req.user;
 
-      if(user.role !== "admin"){
-        throw new Error("You dont permission to do this");
+      if (user.role !== EUserRole.Admin) {
+        throw new Error("You don't permission to do this");
       }
 
       req.user = user;
       next();
-      
     } catch (error) {
-      return errorResponse(res,error);
+      return errorResponse(res, error);
     }
   }
 }
 
-export default new RoleMiddleware()
+export default new RoleMiddleware();
